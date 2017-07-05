c4f27d7e51cb044472badb5d753910ace04bfe9a
Marcin Borowski <marbor112@gmail.com>
2017-07-05 22:44:55 +0200

    Initial commit

---
+++ src/main/java/com/marbor/social/app/SocialAppApplication.java
@@ -0,0 +1,12 @@
+package com.marbor.social.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialAppApplication.class, args);
+	}
+}
